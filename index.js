5b26a8e0289d02f98a6d547dbc86d410a37a1157
Henry Kam <h3nrykam@gmail.com>
2021-10-15 23:26:49 -0700

    finalize io features

--- index.js
+++ index.js
@@ -1,37 +1,121 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const gen = require("./lib/generateHTML")
+//import employee classes
 var arr = []; // array of employees to be rendered
 
 // prompt for input, create objects here, write to html
-function write(data){
-    let output = gen.generateHTML(data);
+function write(){
+    let output = gen.generateHTML(arr);
     fs.writeFile("./dist/output.html", output, (err)=>
         err ? console.error(err) : console.log('Success!')
     );
 }
-function prompt(){
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "What is your project called?",
-            name: "title"
-        },
-        {
-            type: "list", 
-            message:"What license is your project covered under?",
-            choices: ["Apache 2.0", "GNU GPL v3", "MIT","MPL 2.0"],
-            name: "license"
-        },
 
+function promptManager()
+{
+    
+   /* THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+    WHEN I enter the team manager’s name, employee ID, email address, and office number*/
+    console.log("start manager");
+    return inquirer.prompt([
+        {
+            type: "input", 
+            message:"Please enter the manager's name:",
+            name: "name"
+        },
+        {
+            type: "input", 
+            message:"Please enter the manager's id:",
+            name: "id"
+        },
+        {
+            type: "input", 
+            message:"Please enter the manager's email address:",
+            name: "email"
+        },
+        {
+            type: "input", 
+            message:"Please enter the manager's office number:",
+            name: "info"
+        },
+        
     ])
     .then((response)=> {
-        write(response);
+        // create object here, push to array
+        console.log("finish manager");
+        promptEmployee();
+    });
+       
+}
+
+
+function promptEmployee(){
+    console.log("start employee");
+    inquirer.prompt([
+        {
+            type: "input", 
+            message:"Please enter the employee's name:",
+            name: "name"
+        },
+
+        {
+            type: "list", 
+            message:"Please choose your employee's role",
+            choices: ["Engineer", "Intern"],
+            name: "employeeType"
+        },
+
+        {
+            type: "input", 
+            message:"Please enter the employee's id:",
+            name: "id"
+        },
+
+        {
+            type: "input", 
+            message:"Please enter the employee's email address:",
+            name: "email"
+        },
+        
+        // utilizing when in the inquirer docs
+        {
+            type: "input", 
+            message:"Please enter the engineer's github username:",
+            when: (input) => input.employeeType == "Engineer",
+            name: "info"
+        },
+        {
+            type: "input", 
+            message:"Please enter the intern's school:",
+            when: (input) => input.employeeType == "Intern",
+            name: "info"
+        },
+        {
+            type: 'confirm',
+            message: 'Would you like to add more team members?',
+            default: false,
+            name: 'more',
+        }
+        
+    ])
+    .then((response)=> {
+        // create object here, push to array
+        console.log(response);
+        if(response.more)
+            promptEmployee();
+        else
+            write();
     });
 }
 
+
+
+
+
 function init(){
-    prompt();
+    console.log("\n\n\nsomeother intro stuff\n");
+    promptManager();
 }
 
 
